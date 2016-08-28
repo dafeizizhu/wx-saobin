6e8a2bcb666323dc891b7832b74e0f09039de9bd
maizhiying <zhiying.mai@gmail.com>
2016-08-28 23:43:45 +0800

    增加部署到gh-pages的脚本

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp')
+var path = require('path')
+var del = require('del')
+var exec = require('child_process').exec
+
+gulp.task('clean', () => del([path.join(__dirname, 'dist', '**', '*')]))
+
+gulp.task('webpack', ['clean'], cb => {
+  exec('webpack -p', (err, stdout, stderr) => {
+    console.log(stdout)
+    console.log(stderr)
+    cb(err)
+  })
+})
+
+gulp.task('deploy', ['webpack'], () => {
+  gulp.src(['index.html', 'dist/**/*'], {base: '.'})
+    .pipe(gulp.dest(process.env.WX_SAOBIN_DEPLOY_PATH))
+})
+
+gulp.task('default', ['deploy'])
